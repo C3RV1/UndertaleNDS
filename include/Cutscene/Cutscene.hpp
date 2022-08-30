1861c254632874f1cbfcd1c156f10c0a207701b0
Cervi <cervilla.alex@gmail.com>
2022-08-30 02:27:10 +0200

    cutscene progress, structure little change (correct)

--- include/Cutscene/Cutscene.hpp
+++ include/Cutscene/Cutscene.hpp
@@ -10,6 +10,34 @@
 #include <stdio.h>
 #include "Navigation.hpp"
 
+enum CutsceneCommands {
+    LOAD_SPRITE = 0,
+    PLAYER_CONTROL = 1,
+    WAIT_LOAD = 2,
+    SHOW = 3,
+    HIDE = 4,
+    SET_ANIMATION = 5,
+    WAIT_FRAMES = 6,
+    SET_X_Y = 7,
+    MOVE_IN_FRAMES = 8,
+    START_DIALOGUE = 9,
+    WAIT_DIALOGUE_END = 10,
+    START_BATTLE = 11,
+    EXIT_BATTLE = 12,
+    START_BATTLE_DIALOGUE = 13,
+    BATTLE_ATTACK = 14,
+    WAIT_BATTLE_ATTACK = 15,
+    WAIT_BATTLE_ACTION = 16,
+    CMP_BATTLE_ACTION = 17,
+    CHECK_HIT = 18,
+    JUMP_IF = 19,
+    JUMP_IF_NOT = 20,
+    JUMP = 21,
+    MANUAL_CAMERA = 22,
+    UNLOAD_SPRITE = 23,
+    DEBUG = 0xff
+};
+
 class Cutscene {
 public:
     Cutscene(uint16_t cutsceneId, Navigation* navigation);
--- include/Formats/CSCN.hpp
+++ include/Formats/CSCN.hpp
@@ -5,4 +5,13 @@
 #ifndef LAYTON_CSCN_HPP
 #define LAYTON_CSCN_HPP
 
+#include <stdint.h>
+
+struct CSCN {
+    char header[4] = {'C', 'S', 'C', 'N'};
+    uint32_t version = 1;
+    uint32_t fileSize = 0;
+    uint8_t* commands = nullptr;
+};
+
 #endif //LAYTON_CSCN_HPP
