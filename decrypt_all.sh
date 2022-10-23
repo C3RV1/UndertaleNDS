c0ee504d2e5bfa54149939819c90bfc406ab9b03
Cervi <cervilla.alex@gmail.com>
2022-10-23 10:40:26 +0200

    remove old distribution files

--- decrypt_all.sh
+++
@@ -1,4 +0,0 @@
-python3 -m pip install pycryptodome
-python3 tools/decrypt_files.py ../Undertale.nds.aes ../Undertale.nds
-python3 tools/decrypt_files.py ../nitrofs.zip.aes ../nitrofs
-python3 tools/decrypt_files.py ../tools.zip.aes ../tools
--- encrypt_all.sh
+++
@@ -1,6 +0,0 @@
-python3 tools/all.py
-make
-python3 -m pip install pycryptodome
-python3 tools/encrypt_files.py ../Undertale.nds ../Undertale.nds.aes
-python3 tools/encrypt_files.py ../nitrofs ../nitrofs.zip.aes
-python3 tools/encrypt_files.py ../tools ../tools.zip.aes
