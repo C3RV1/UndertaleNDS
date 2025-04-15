5b91a1eafca3dc8ff5714725714cb92842a0f1be
Àlex Cervilla <cervilla.alex@gmail.com>
2025-04-15 13:45:23 +0200

    better rock collision and moving

--- source/Engine/math.cpp
+++ source/Engine/math.cpp
@@ -11,7 +11,7 @@ bool collidesRect(int x1, int y1, int w1, int h1, int x2, int y2, int w2,
 
 bool rectContainsOther(int x1, int y1, int w1, int h1, int x2, int y2, int w2,
                        int h2) {
-  return (x2 > x1 && y2 > y1 && x2 + w2 < x1 + w1 && y2 + h2 < y1 + h1);
+  return (x2 >= x1 && y2 >= y1 && x2 + w2 <= x1 + w1 && y2 + h2 <= y1 + h1);
 }
 
 int distSquared(int x1, int y1, int x2, int y2) {
